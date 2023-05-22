3a9e1be0264570a4173bcfd8a299941288922e0d
MiguelRodriguezS1AM <miguel.rodriguez@sarria.salesians.cat>
2023-05-22 17:25:49 +0200

    Obstaculos, si

--- Obstacle Odyssey/Assets/Scripts/BlockSpawnController.cs
+++ Obstacle Odyssey/Assets/Scripts/BlockSpawnController.cs
@@ -8,25 +8,37 @@ public class BlockSpawnController : MonoBehaviour
     public GameObject punto;
     public float spawnRate = 1.0f;
     public float spawnX = -5.0f;
-    public float spawnY = 0.0f; // Coordenada Y donde se generarán los objetos
+    public float spawnY = 0.0f;
+    public float spawnPointY = 0.0f;
 
     private float nextSpawnTime = 0.0f;
+    private int cont = 0;
+    private int index = 0;
 
     void Update()
     {
         if (Time.time >= nextSpawnTime)
         {
             // Seleccionar un bloque aleatorio de la lista
-            int index = Random.Range(0, blockPrefabs.Count);
+            if (cont < 5)
+            {
+                index = Random.Range(0, 3);
+            }
+            else
+            {
+                index = Random.Range(0, blockPrefabs.Count);
+            }
 
             // Instanciar el bloque en la posición especificada por spawnX y spawnY
             Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0.0f);
             Instantiate(blockPrefabs[index], spawnPosition, Quaternion.identity);
 
-            Instantiate(punto, spawnPosition, Quaternion.identity);
+            Vector3 spawnPositionPoint = new Vector3(spawnX, spawnPointY, 0.0f);
+            Instantiate(punto, spawnPositionPoint, Quaternion.identity);
 
             // Establecer el próximo tiempo de aparición
             nextSpawnTime = Time.time + spawnRate;
+            cont++;
         }
     }
 }
