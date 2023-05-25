ef0388bec0b58b0ea569ab04fd9c79bc2959309f
JOSEPFIESTAS <jose.fiestas@sarria.salesians.cat>
2023-05-25 19:19:13 +0200

    frkfro

--- Obstacle Odyssey/Assets/Scripts/ScoreManager.cs
+++ Obstacle Odyssey/Assets/Scripts/ScoreManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,8 +8,10 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
 
     public Text scoreText;
+    public Text highScoreText;
 
     int score = 0;
+    int highScore = 0;
 
     private void Awake() 
     {
@@ -18,12 +20,18 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        highScore = PlayerPrefs.GetInt("HighSocre: ", 0);
         scoreText.text = score.ToString();
+        highScoreText.text = "HIGHSCORE: " + highScore.ToString();
     }
 
     public void AddPoint()
     {
         score += 1;
         scoreText.text = score.ToString();
+        if(highScore < score)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
